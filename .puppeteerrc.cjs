d1426c1bb7186d776a6460d68c34cfc2393ba20c
VishnuGadekar7 <vishnugadekar7@gmail.com>
2026-08-28 02:17:31 +0530

    fix: copy exact approach from energize-form with zero custom overrides

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -2,5 +2,5 @@ const { join } = require("path");
 
 /** @type {import("puppeteer").Configuration} */
 module.exports = {
-  cacheDirectory: join(__dirname, ".browser-cache"),
+  cacheDirectory: join(__dirname, ".cache", "puppeteer"),
 };
